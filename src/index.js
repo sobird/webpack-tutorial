ce5b1681103592d5b4a55557b86311f9be25b039
sobird <i@sobird.me>
2019-11-06 21:01:07 +0800

    管理资源 加载css 加载images图像

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@
  * sobird<i@sobird.me> at 2019-11-06 18:23:36 build.
  */
 
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.svg';
+
 function component() {
   let element = document.createElement('div');
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // 将图像添加到我们已经存在的div中
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,24 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '../dist/',
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+    ]
   }
 };
