1533e736b9b9f1730cd6775e42436566f1f3a0ef
sobird <i@sobird.me>
2019-11-08 12:02:01 +0800

    动态导入async函数

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@
  * sobird<i@sobird.me> at 2019-11-06 18:23:36 build.
  */
 
-function getComponent() {
-  return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    return element;
-  }).catch(error => 'An error occurred while loading the component');
+async function getComponent() {
+  var element = document.createElement('div');
+  const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return element;
 }
 
 getComponent().then(component => {
