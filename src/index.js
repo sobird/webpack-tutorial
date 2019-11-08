2e73b681e1b98704ca604f565c23da21f75edc26
sobird <i@sobird.me>
2019-11-08 13:26:33 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -4,13 +4,28 @@
  * sobird<i@sobird.me> at 2019-11-06 18:23:36 build.
  */
 
-async function getComponent() {
+import _ from 'lodash';
+
+function component() {
   var element = document.createElement('div');
-  const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  var button = document.createElement('button');
+  var br = document.createElement('br');
+
+  button.innerHTML = 'Click me and look at the console!';
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default;
+
+    print();
+
+  });
+
   return element;
 }
 
-getComponent().then(component => {
-  document.body.appendChild(component);
-})
+document.body.appendChild(component());
--- src/print.js
+++ src/print.js
@@ -4,6 +4,8 @@
  * sobird<i@sobird.me> at 2019-11-06 21:30:27 build.
  */
 
-export default function printMe() {
-  console.log('I get called from print.js!');
-}
+console.log('The print.js module has loaded! See the network tab in dev tools...');
+
+export default () => {
+  console.log('Button Clicked: Here\'s "some text"!');
+};
