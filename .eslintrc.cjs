9fc3bcce05d256d43695f5ffa0d2aad4705d7ccb
sobird <i@sobird.me>
2023-11-21 21:34:43 +0800

    use eslit-config-sobird

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,6 @@
-// .eslintrc.js
- 
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
   extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
+    'sobird/typescript-react.cjs',
   ],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  rules: {
-    // 在这里添加需要覆盖的规则
-  }
 };
--- src/test.tsx
+++ src/test.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
-function Test() {
-  return (
-    <div>
-      Test
-    </div>
-  );
-}
-
-export default Test;
+export default () => (
+  <div>
+    Test
+  </div>
+);
