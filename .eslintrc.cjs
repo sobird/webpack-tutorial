9265b3156094f170686e0db977874472db27cd44
sobird <i@sobird.me>
2023-11-21 20:13:32 +0800

    config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,56 +1,18 @@
-/**
- * "off" or 0 - turn the rule off
- * "warn" or 1 - turn the rule on as a warning (doesn't affect exit code)
- * "error" or 2 - turn the rule on as an error (exit code will be 1)
- * 
- * @see https://typescript-eslint.io/play
- */
-
+// .eslintrc.js
+ 
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    // 'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
+  root: true,
   parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+  ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint', 'react'],
   rules: {
-    // 'indent': ['error', 2],
-    '@typescript-eslint/indent': ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    '@typescript-eslint/semi': 'error',
-    'no-unused-vars': ['warn'],
-    'no-multi-spaces': 'error',
-    '@typescript-eslint/no-unused-vars': ['warn'],
-    '@typescript-eslint/space-before-blocks': 'error',
-    '@typescript-eslint/comma-spacing': ['error', { 'before': false, 'after': true }],
-    '@typescript-eslint/object-curly-spacing': ['error', 'always'],
-    '@typescript-eslint/type-annotation-spacing': 'error',
-    '@typescript-eslint/keyword-spacing': 'error',
-    '@typescript-eslint/key-spacing': 'error',
-    '@typescript-eslint/func-call-spacing': 'error',
-    '@typescript-eslint/block-spacing': 'error'
-  },
+    // 在这里添加需要覆盖的规则
+  }
 };
